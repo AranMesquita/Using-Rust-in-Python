2bc5c7073d8167142b24062e4ff4312a9537bfc6
AranMesquita <aranmesquita@gmail.com>
2025-07-20 10:09:37 +0200

    updated runtime_bench_mark.p file

--- src-py/runtime_bench_mark.py
+++ src-py/runtime_bench_mark.py
@@ -1,9 +1,12 @@
 import timeit
 from benchmark_funcs import python_runtime_benchmark, rust_runtime_benchmark  # noqa: F401
 
-# reason why we only have NTH_TERM = 93 as 93rd Fibonacci number is the largest that fits in a u64
+# reason why we only have NTH_TERM = 94 as 93rd Fibonacci number is the largest that fits in a u64
 # and 94th Fibonacci number is the first that exceeds u64::MAX
-NTH_TERM = 93
+# the rust_runtime_benchmark & the python_runtime_benchmark functions
+# will run from 0 to NTH_TERM - 1, so we set NTH_TERM to 94
+# to benchmark calculating 0 --> 93rd Fibonacci number
+NTH_TERM = 94
 NUMBER_OF_CALLS = 100000
 
 rust_time = timeit.timeit(
